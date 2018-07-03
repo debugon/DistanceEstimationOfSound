4f44408ed02e12721ad5a6b7dea6574e16570dde
Debugon <games0421@gmail.com>
2018-07-03 19:46:51 +0900

    ボリュームに合わせてゲージの長さが変わるように変更

--- Assets/Scripts/VolumeToScale.cs
+++ Assets/Scripts/VolumeToScale.cs
@@ -6,16 +6,19 @@ using System.Linq;
 public class VolumeToScale : MonoBehaviour {
 
     private float[] waveData = new float[1024];
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
         AudioListener.GetOutputData(waveData, 1);
         var volume = waveData.Select(x => x * x).Sum() / waveData.Length;
-        transform.localScale = Vector3.one * volume;
-	}
+        //transform.localScale = new Vector3(1, volume * 100, 1);
+        GetComponent<RectTransform>().sizeDelta = new Vector2(100, volume * 100000);
+        GetComponent<RectTransform>().localPosition = new Vector3(0, GetComponent<RectTransform>().sizeDelta.y / 2 - 50, 0);
+        Debug.Log((GetComponent<RectTransform>().sizeDelta.y / 2.0f) );
+    }
 }
