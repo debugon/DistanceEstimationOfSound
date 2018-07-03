f950c75d1eee325a987f8420c52fa2fd5a90e0b7
Debugon <games0421@gmail.com>
2018-07-03 16:50:17 +0900

    AudioListenerで取得した音をアタッチしたオブジェクトのサイズに変換する

---
+++ Assets/Scripts/VolumeToScale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class VolumeToScale : MonoBehaviour {
+
+    private float[] waveData = new float[1024];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AudioListener.GetOutputData(waveData, 1);
+        var volume = waveData.Select(x => x * x).Sum() / waveData.Length;
+        transform.localScale = Vector3.one * volume;
+	}
+}
